d1d9eef19f0c6c4eb4bac3e15b69b611c1a39298
Goke Oladokun <gokeladokun@hotmail.com>
2026-08-03 00:19:37 +0100

    Add Goke.Bank.Web

--- GokeBankApp.AppHost/AppHost.cs
+++ GokeBankApp.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Goke_Bank_WebServer>("goke-bank-webserver");
 
+builder.AddProject<Projects.Goke_Bank_Web>("goke-bank-web");
+
 builder.Build().Run();
